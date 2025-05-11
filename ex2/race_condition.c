c9ed0c1004c481c76f707af3ef0bbc4b5893f1e1
AbdullahHamad04 <abdullahhamd222@gmail.com>
2025-05-11 09:20:23 +0200

    Update r.c

--- ex2/race_condition.c
+++ ex2/race_condition.c
@@ -1,38 +1,38 @@
-#include <alchemy/task.h>
-#include <alchemy/mutex.h>
 #include <stdio.h>
-#include <unistd.h>
+#include <alchemy/task.h>
+#include <alchemy/sem.h>
 
-RT_TASK task1, task2;
-RT_MUTEX lock;
-int shared_counter = 0;
+RT_TASK t1, t2;
+RT_SEM  mutex;
 
-void task_func(void *arg) {
-    char *name = (char *)arg;
+void sem_task(void *arg)
+{
+    int id = (int)(long)arg;
+    for (int i = 0; i < 3; ++i) {
+        //rt_sem_p(&mutex, TM_INFINITE); 
 
-    rt_mutex_acquire(&lock, TM_INFINITE); // lock
+        rt_printf("Task %d entering critical section\n", id);
+        rt_task_sleep(1000000000);        /* 1 s */
+        rt_printf("Task %d leaving critical section\n", id);
 
-    for (int i = 0; i < 10000; i++) {
-        shared_counter++;
+        //rt_sem_v(&mutex);   
+        rt_task_sleep(500000000);         /* 0.5 s */
     }
-
-    rt_mutex_release(&lock); // unlock
-
-    printf("%s done. shared_counter = %d\n", name, shared_counter);
 }
 
-int main() {
-    printf("starting mutex demo...\n");
+int main(void)
+{
+    /* 1) Create a mutex semaphore (initial count 1) */
+    rt_sem_create(&mutex, "mutex", 1, S_PRIO);
 
-    rt_mutex_create(&lock, "SharedLock");
+    /* 2) Two tasks at same priority to “fight” for the semaphore */
+    rt_task_create(&t1, "sem1", 0, 40, 0);
+    rt_task_create(&t2, "sem2", 0, 40, 0);
 
-    rt_task_create(&task1, "Task1", 0, 50, 0);
-    rt_task_create(&task2, "Task2", 0, 50, 0);
-
-    rt_task_start(&task1, &task_func, "Task1");
-    rt_task_start(&task2, &task_func, "Task2");
+    /* 3) Start them */
+    rt_task_start(&t1, &sem_task, (void*)(long)1);
+    rt_task_start(&t2, &sem_task, (void*)(long)2);
 
     pause();
     return 0;
 }
-
